ecd1b68b5a3eebc07bec614baeb4fc46384cb51b
kooky <kookyzhang99@gmail.com>
2016-03-25 13:25:11 +0200

    Feat: Add new Feature for google login

--- restore.sh
+++ restore.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Set start and end date
-START_DATE="2015-02-7"  # YYYY-MM-DD
-END_DATE="2015-02-24"    # YYYY-MM-DD
+START_DATE="2016-03-2"  # YYYY-MM-DD
+END_DATE="2016-03-28"    # YYYY-MM-DD
 
 # Min and max commits per selected day
-MIN_COMMITS=1
-MAX_COMMITS=1
+MIN_COMMITS=4
+MAX_COMMITS=7
 
 # Min and max days per month to commit
-MIN_DAYS_PER_MONTH=1
-MAX_DAYS_PER_MONTH=1
+MIN_DAYS_PER_MONTH=8
+MAX_DAYS_PER_MONTH=16
 
 # Set author and committer name and email
 AUTHOR_NAME="kooky"
